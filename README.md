f30ca90e71602081d86c2119f58d3baf613e3d6a
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2024-06-20 01:53:32 +0100

    start of documentation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Swimlink Documentation
+
+This documentation is to be used to onboard new admin users to the Swimlink platform.
---
+++ theme.config.jsx
@@ -0,0 +1,58 @@
+import { useConfig } from "nextra-theme-docs";
+
+export default {
+  logo: (
+    <img
+      src="https://res.cloudinary.com/luvely/image/upload/v1712853154/NavbarLogo1_kit76u.png"
+      alt="Logo"
+      style={{
+        width: "100%",
+        height: "30px",
+      }}
+    />
+  ),
+  project: {
+    link: "https://github.com/hokagedemehin/swimlink-web-docs",
+  },
+  docsRepositoryBase: "https://github.com/hokagedemehin/swimlink-web-docs",
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Swimlink Web Documentation" />
+      <meta name="og:title" content="Swimlink Web Documentation" />
+    </>
+  ),
+  navigation: {
+    prev: true,
+    next: true,
+  },
+  gitTimestamp: () => {
+    const { gitTimestamp } = useConfig();
+    return (
+      <div>
+        <p>Last Updated: {gitTimestamp}</p>
+      </div>
+    );
+  },
+  footer: {
+    text: (
+      <>
+        <div className="">
+          Copyright {new Date().getFullYear()} ©{" "}
+          <a
+            href="https://admin-stg-f987f871eac0.herokuapp.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Swimlink
+          </a>
+        </div>
+        {/* <div>
+          <a href="/" target="_blank" rel="noopener noreferrer">
+            Swimlink Web Documentation
+          </a>
+        </div> */}
+      </>
+    ),
+  },
+};
